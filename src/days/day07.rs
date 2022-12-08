28af2e21e5379d7bfb994e27e667d3b7ccb00592
JakeHuneau <jakehuneau@yahoo.com>
2022-12-08 11:39:06 -0500

    day8

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -11,184 +11,184 @@ use std::{
 };
 
 pub fn solve() -> SolutionPair {
-    let mut filestructure = FileObject {
-        name: String::from("/"),
-        size: 0,
-        parent: None,
-        children: vec![],
-    };
-    // let mut head = Rc::new(&filestructure);
-    let mut current_directory = Rc::new(RefCell::new(filestructure));
-    read_to_string("input/day7")
-        .expect("Could not read file")
-        .lines()
-        .for_each(|line| {
-            let line_parts = line.split_whitespace().collect::<Vec<&str>>();
-            if line_parts[0] == "$" && line_parts[1] == "cd" && line_parts[2] != "/" {
-                match current_directory.into_inner().cd(line_parts[2]) {
-                    Some(new_directory) => current_directory = new_directory,
-                    None => {}
-                }
-            } else {
-                // Must be a fileobject description
-                current_directory
-                    .get_mut()
-                    .add_child(line.parse::<FileObject>().unwrap());
-            }
-        });
+    // let mut filestructure = FileObject {
+    //     name: String::from("/"),
+    //     size: 0,
+    //     parent: None,
+    //     children: vec![],
+    // };
+    // // let mut head = Rc::new(&filestructure);
+    // let mut current_directory = Rc::new(RefCell::new(filestructure));
+    // read_to_string("input/day7")
+    //     .expect("Could not read file")
+    //     .lines()
+    //     .for_each(|line| {
+    //         let line_parts = line.split_whitespace().collect::<Vec<&str>>();
+    //         if line_parts[0] == "$" && line_parts[1] == "cd" && line_parts[2] != "/" {
+    //             match current_directory.into_inner().cd(line_parts[2]) {
+    //                 Some(new_directory) => current_directory = new_directory,
+    //                 None => {}
+    //             }
+    //         } else {
+    //             // Must be a fileobject description
+    //             current_directory
+    //                 .get_mut()
+    //                 .add_child(line.parse::<FileObject>().unwrap());
+    //         }
+    //     });
     // println!("{:?}", current_directory.get_mut().name);
     (Solution::UInt(1), Solution::UInt(1))
 }
 
-struct FileObject {
-    name: String,
-    size: usize,
-    parent: Option<Rc<RefCell<FileObject>>>,
-    children: Vec<Rc<RefCell<FileObject>>>,
-}
+// struct FileObject {
+//     name: String,
+//     size: usize,
+//     parent: Option<Rc<RefCell<FileObject>>>,
+//     children: Vec<Rc<RefCell<FileObject>>>,
+// }
 
-impl FromStr for FileObject {
-    type Err = ();
+// impl FromStr for FileObject {
+//     type Err = ();
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut split = s.split_whitespace().collect::<Vec<&str>>();
-        match split[0] == "dir" {
-            true => Ok(FileObject::new(String::from(split[1]), 0)),
-            false => Ok(FileObject::new(
-                String::from(split[1]),
-                split[0].parse::<usize>().unwrap(),
-            )),
-        }
-    }
-}
+//     fn from_str(s: &str) -> Result<Self, Self::Err> {
+//         let mut split = s.split_whitespace().collect::<Vec<&str>>();
+//         match split[0] == "dir" {
+//             true => Ok(FileObject::new(String::from(split[1]), 0)),
+//             false => Ok(FileObject::new(
+//                 String::from(split[1]),
+//                 split[0].parse::<usize>().unwrap(),
+//             )),
+//         }
+//     }
+// }
 
-impl FileObject {
-    fn new(name: String, size: usize) -> Self {
-        Self {
-            name,
-            size,
-            parent: None,
-            children: vec![],
-        }
-    }
+// impl FileObject {
+//     fn new(name: String, size: usize) -> Self {
+//         Self {
+//             name,
+//             size,
+//             parent: None,
+//             children: vec![],
+//         }
+//     }
 
-    fn get_size(&self) -> usize {
-        self.size
-            + self
-                .children
-                .iter()
-                .map(|child| child.get_mut().get_size())
-                .sum::<usize>()
-    }
+//     fn get_size(&self) -> usize {
+//         self.size
+//             + self
+//                 .children
+//                 .iter()
+//                 .map(|child| child.get_mut().get_size())
+//                 .sum::<usize>()
+//     }
 
-    fn cd(&self, new_location: &str) -> Option<Rc<RefCell<FileObject>>> {
-        if new_location == ".." {
-            return match &self.parent {
-                Some(parent) => Some(parent.to_owned()),
-                None => None,
-            };
-        }
-        for dir in &self.children {
-            if dir.get_mut().name == new_location {
-                return Some(dir.to_owned());
-            }
-        }
-        None
-    }
+//     fn cd(&self, new_location: &str) -> Option<Rc<RefCell<FileObject>>> {
+//         if new_location == ".." {
+//             return match &self.parent {
+//                 Some(parent) => Some(parent.to_owned()),
+//                 None => None,
+//             };
+//         }
+//         for dir in &self.children {
+//             if dir.get_mut().name == new_location {
+//                 return Some(dir.to_owned());
+//             }
+//         }
+//         None
+//     }
 
-    fn add_parent(&mut self, parent: Rc<RefCell<FileObject>>) {
-        self.parent = Some(parent);
-    }
+//     fn add_parent(&mut self, parent: Rc<RefCell<FileObject>>) {
+//         self.parent = Some(parent);
+//     }
 
-    fn add_child(&mut self, child: FileObject) {
-        self.children.push(Rc::new(RefCell::new(child)));
-    }
-}
+//     fn add_child(&mut self, child: FileObject) {
+//         self.children.push(Rc::new(RefCell::new(child)));
+//     }
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn test_dir_size_no_child_dir() {
-        let dir = FileObject {
-            name: String::from("dir"),
-            size: 0,
-            parent: None,
-            children: vec![
-                Rc::new(RefCell::new(FileObject::new(String::from("a"), 1))),
-                Rc::new(RefCell::new(FileObject::new(String::from("b"), 2))),
-            ],
-        };
-        assert_eq!(dir.get_size(), 3);
-    }
+//     #[test]
+//     fn test_dir_size_no_child_dir() {
+//         let dir = FileObject {
+//             name: String::from("dir"),
+//             size: 0,
+//             parent: None,
+//             children: vec![
+//                 Rc::new(RefCell::new(FileObject::new(String::from("a"), 1))),
+//                 Rc::new(RefCell::new(FileObject::new(String::from("b"), 2))),
+//             ],
+//         };
+//         assert_eq!(dir.get_size(), 3);
+//     }
 
-    #[test]
-    fn test_dir_size_with_child_dir() {
-        let dir = FileObject {
-            name: String::from("dir1"),
-            size: 0,
-            parent: None,
-            children: vec![
-                Rc::new(RefCell::new(FileObject::new(String::from("a"), 1))),
-                Rc::new(RefCell::new(FileObject::new(String::from("b"), 2))),
-                Rc::new(RefCell::new(FileObject {
-                    name: String::from("child"),
-                    size: 0,
-                    parent: None,
-                    children: vec![
-                        Rc::new(RefCell::new(FileObject::new(String::from("c"), 3))),
-                        Rc::new(RefCell::new(FileObject::new(String::from("d"), 4))),
-                    ],
-                })),
-            ],
-        };
-        assert_eq!(dir.get_size(), 10);
-    }
+//     #[test]
+//     fn test_dir_size_with_child_dir() {
+//         let dir = FileObject {
+//             name: String::from("dir1"),
+//             size: 0,
+//             parent: None,
+//             children: vec![
+//                 Rc::new(RefCell::new(FileObject::new(String::from("a"), 1))),
+//                 Rc::new(RefCell::new(FileObject::new(String::from("b"), 2))),
+//                 Rc::new(RefCell::new(FileObject {
+//                     name: String::from("child"),
+//                     size: 0,
+//                     parent: None,
+//                     children: vec![
+//                         Rc::new(RefCell::new(FileObject::new(String::from("c"), 3))),
+//                         Rc::new(RefCell::new(FileObject::new(String::from("d"), 4))),
+//                     ],
+//                 })),
+//             ],
+//         };
+//         assert_eq!(dir.get_size(), 10);
+//     }
 
-    #[test]
-    fn test_add_child() {
-        let mut parent = FileObject::new(String::from("parent"), 0);
-        let child = FileObject::new(String::from("child"), 0);
-        parent.add_child(child);
-        assert_eq!(parent.children[0].get_mut().name, "child");
-    }
+//     #[test]
+//     fn test_add_child() {
+//         let mut parent = FileObject::new(String::from("parent"), 0);
+//         let child = FileObject::new(String::from("child"), 0);
+//         parent.add_child(child);
+//         assert_eq!(parent.children[0].get_mut().name, "child");
+//     }
 
-    #[test]
-    fn test_add_parent() {
-        let parent = FileObject::new(String::from("parent"), 0);
-        let mut child = FileObject::new(String::from("child"), 0);
-        child.add_parent(Rc::new(RefCell::new(parent)));
-        assert_eq!(child.parent.unwrap().get_mut().name, "parent");
-    }
+//     #[test]
+//     fn test_add_parent() {
+//         let parent = FileObject::new(String::from("parent"), 0);
+//         let mut child = FileObject::new(String::from("child"), 0);
+//         child.add_parent(Rc::new(RefCell::new(parent)));
+//         assert_eq!(child.parent.unwrap().get_mut().name, "parent");
+//     }
 
-    #[test]
-    fn test_cd_down() {
-        let dir = FileObject {
-            name: String::from("parent"),
-            size: 0,
-            parent: None,
-            children: vec![Rc::new(RefCell::new(FileObject::new(
-                String::from("child"),
-                0,
-            )))],
-        };
-        let mut current_dir = Rc::new(RefCell::new(dir));
-        current_dir = current_dir.get_mut().cd("child").unwrap();
-        assert_eq!(*current_dir.get_mut().name, String::from("child"));
-    }
+//     #[test]
+//     fn test_cd_down() {
+//         let dir = FileObject {
+//             name: String::from("parent"),
+//             size: 0,
+//             parent: None,
+//             children: vec![Rc::new(RefCell::new(FileObject::new(
+//                 String::from("child"),
+//                 0,
+//             )))],
+//         };
+//         let mut current_dir = Rc::new(RefCell::new(dir));
+//         current_dir = current_dir.get_mut().cd("child").unwrap();
+//         assert_eq!(*current_dir.get_mut().name, String::from("child"));
+//     }
 
-    #[test]
-    fn test_cd_up() {
-        let parent = FileObject::new(String::from("parent"), 0);
-        let mut child = FileObject {
-            name: String::from("child"),
-            size: 0,
-            parent: Some(Rc::new(RefCell::new(parent))),
-            children: vec![],
-        };
-        let mut current_dir = Rc::new(RefCell::new(child));
-        current_dir = current_dir.get_mut().cd("..").unwrap();
-        assert_eq!(*current_dir.get_mut().name, String::from("parent"));
-    }
-}
+//     #[test]
+//     fn test_cd_up() {
+//         let parent = FileObject::new(String::from("parent"), 0);
+//         let mut child = FileObject {
+//             name: String::from("child"),
+//             size: 0,
+//             parent: Some(Rc::new(RefCell::new(parent))),
+//             children: vec![],
+//         };
+//         let mut current_dir = Rc::new(RefCell::new(child));
+//         current_dir = current_dir.get_mut().cd("..").unwrap();
+//         assert_eq!(*current_dir.get_mut().name, String::from("parent"));
+//     }
+// }
--- src/days/day08.rs
+++ src/days/day08.rs
@@ -3,5 +3,118 @@ use crate::{Solution, SolutionPair};
 use std::fs::read_to_string;
 
 pub fn solve() -> SolutionPair {
-    todo!()
+    let mut total = 0;
+    let grid = read_to_string("input/day8")
+        .expect("Could not read file")
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|ch| ch.to_string().parse::<u8>().unwrap())
+                .collect::<Vec<u8>>()
+        })
+        .collect::<Vec<Vec<u8>>>();
+
+    let mut max_tree_count = 0;
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            let this_count = count_trees(&grid, i, j);
+            if this_count > max_tree_count {
+                max_tree_count = this_count;
+            }
+            if is_visible(&grid, i, j) {
+                total += 1;
+            }
+        }
+    }
+    (Solution::UInt(total), Solution::UInt(max_tree_count))
+}
+
+fn is_visible(grid: &Vec<Vec<u8>>, row: usize, column: usize) -> bool {
+    let this_val = grid[row][column];
+    if row == 0 || row == grid.len() - 1 || column == 0 || column == grid[0].len() - 1 {
+        return true;
+    }
+    // Need to check every tree in a direction
+    let mut visible_left = true;
+    for c in (0..column).rev() {
+        if this_val <= grid[row][c] {
+            visible_left = false;
+            break;
+        }
+    }
+    if visible_left {
+        return true;
+    }
+
+    let mut visible_right = true;
+    for c in column + 1..grid.len() {
+        if this_val <= grid[row][c] {
+            visible_right = false;
+            break;
+        }
+    }
+    if visible_right {
+        return true;
+    }
+
+    let mut visible_up = true;
+    for r in (0..row).rev() {
+        if this_val <= grid[r][column] {
+            visible_up = false;
+            break;
+        }
+    }
+    if visible_up {
+        return true;
+    }
+
+    let mut visible_down = true;
+    for r in row + 1..grid[0].len() {
+        if this_val <= grid[r][column] {
+            visible_down = false;
+            break;
+        }
+    }
+    if visible_down {
+        return true;
+    }
+    false
+}
+
+fn count_trees(grid: &Vec<Vec<u8>>, row: usize, column: usize) -> usize {
+    let this_val = grid[row][column];
+
+    // Need to check every tree in a direction
+    let mut left_count = 0;
+    for c in (0..column).rev() {
+        left_count += 1;
+        if this_val <= grid[row][c] {
+            break;
+        }
+    }
+
+    let mut right_count = 0;
+    for c in column + 1..grid.len() {
+        right_count += 1;
+        if this_val <= grid[row][c] {
+            break;
+        }
+    }
+
+    let mut up_count = 0;
+    for r in (0..row).rev() {
+        up_count += 1;
+        if this_val <= grid[r][column] {
+            break;
+        }
+    }
+
+    let mut down_count = 0;
+    for r in row + 1..grid[0].len() {
+        down_count += 1;
+        if this_val <= grid[r][column] {
+            break;
+        }
+    }
+    left_count * right_count * up_count * down_count
 }
